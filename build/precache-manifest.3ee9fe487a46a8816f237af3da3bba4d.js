34ec9d4d482d6265b71219015a10b977f3be606c
prathamtangri1 <prathamtangri1@gmail.com>
2020-06-25 23:25:33 +0530

    Changes to make it Heroku compatible

---
+++ build/precache-manifest.3ee9fe487a46a8816f237af3da3bba4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b580c98a71cea1c6afbcd31458c712e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "329bee5ce0bf7036d76e",
+    "url": "/static/css/main.85a34b6c.chunk.css"
+  },
+  {
+    "revision": "f40a88c47c2d8a6b2c48",
+    "url": "/static/js/2.74fc3547.chunk.js"
+  },
+  {
+    "revision": "7e402396ca428a5bd514c48df615b24e",
+    "url": "/static/js/2.74fc3547.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "329bee5ce0bf7036d76e",
+    "url": "/static/js/main.dc6fc08c.chunk.js"
+  },
+  {
+    "revision": "bd0a69f83ccbec5df016",
+    "url": "/static/js/runtime-main.4506e877.js"
+  }
+]);
